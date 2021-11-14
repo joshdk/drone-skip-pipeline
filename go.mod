df43032bbeb01d8a09910bc21ccd21f647d97c5f
Josh Komoroske <jdkomo@gmail.com>
2021-11-14 11:41:59 -0800

    feat: load plugin config from env (#6)

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/joshdk/drone-skip-pipeline
 
 go 1.17
 
-require jdk.sh/meta v0.1.1-0.20211021015548-cff3b930ebd9
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	jdk.sh/meta v0.1.1-0.20211021015548-cff3b930ebd9
+)
--- main.go
+++ main.go
@@ -6,9 +6,11 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"os"
 
+	"github.com/kelseyhightower/envconfig"
 	"jdk.sh/meta"
 )
 
@@ -21,5 +23,43 @@ func main() {
 
 func mainCmd() error {
 	log.Printf("joshdk/drone-skip-pipeline %s (%s)\n", meta.Version(), meta.ShortSHA())
+
+	_, err := loadConfig()
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+type config struct {
+	Event       string   `envconfig:"DRONE_BUILD_EVENT"`
+	GithubToken string   `envconfig:"GITHUB_TOKEN"`
+	PullRequest int      `envconfig:"DRONE_PULL_REQUEST"`
+	RepoName    string   `envconfig:"DRONE_REPO_NAME"`
+	RepoOwner   string   `envconfig:"DRONE_REPO_OWNER"`
+	Rules       []string `envconfig:"PLUGIN_RULES"`
+}
+
+func loadConfig() (*config, error) {
+	// Load plugin configuration from current working environment.
+	var cfg config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	// Sanity check that plugin is sufficiently configured.
+	switch {
+	case cfg.Event == "":
+		return nil, errors.New("missing DRONE_BUILD_EVENT")
+	case cfg.PullRequest == 0:
+		return nil, errors.New("missing DRONE_PULL_REQUEST")
+	case cfg.RepoName == "":
+		return nil, errors.New("missing DRONE_REPO_NAME")
+	case cfg.RepoOwner == "":
+		return nil, errors.New("missing DRONE_REPO_OWNER")
+	}
+
+	return &cfg, nil
 }
